3b6ddaed13849c05e384ec642dc8dc2ffd48860b
Haohe Zhu <2452466220@qq.com>
2024-03-11 17:41:30 +0800

    feat: 集成Clerk实现身份认证

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="h-full bg-blue-200">{children}</div>;
+  return (
+    <div className="h-full flex justify-center items-center">{children}</div>
+  );
 };
 
 export default AuthLayout;
