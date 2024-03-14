795991ab5c4345e384df5059d3ccc0e0e4500068
Haohe Zhu <2452466220@qq.com>
2024-03-14 17:53:46 +0800

    feat: prisma & neon setup

---
+++ lib/db.ts
@@ -0,0 +1,10 @@
+//在开发环境中使用单例模式 避免多次初始化和资源浪费
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
