fbb2eb0d712cceaf1fd0b6f103242840014ea056
Haohe Zhu <2452466220@qq.com>
2024-03-19 19:39:42 +0800

    feat: Server Sidebar

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -35,7 +35,7 @@ export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
               "bg-primary/10 text-primary rounded-[16px]"
           )}
         >
-          <Image fill src={imageUrl} alt="Channel" />
+          <Image fill src={imageUrl} alt="Channel" sizes="48" />
         </div>
       </button>
     </ActionTooltip>
