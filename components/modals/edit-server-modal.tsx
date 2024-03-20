30b973dd98c7ef760e4b020dbdaa56bfd1324f60
Haohe Zhu <2452466220@qq.com>
2024-03-20 20:27:58 +0800

    feat: update server

---
+++ components/modals/edit-server-modal.tsx
@@ -0,0 +1,150 @@
+"use client";
+import axios from "axios";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FileUpload } from "@/components/file-upload";
+import { useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-modal-store";
+import { useEffect } from "react";
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "服务器名称是必填项!",
+  }),
+  imageUrl: z.string().min(1, {
+    message: "图片链接是必填项!",
+  }),
+});
+
+export const EditServerModal = () => {
+  const { type, isOpen, onClose, data } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = type === "editServer" && isOpen;
+  const { server } = data;
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  useEffect(() => {
+    if (server) {
+      form.setValue("name", server.name);
+      form.setValue("imageUrl", server.imageUrl);
+    }
+  }, [server, form]);
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/servers/${server?.id}`, values);
+
+      form.reset();
+      router.refresh();
+      onClose();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center tracking-widest">
+            定制您的专属服务器
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            为您的服务器赋予一个有个性的名字和头像。您可以随时更改它。
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="mt-8 mb-8 px-6">
+              <div className="flex items-center justify-center text-center mb-8">
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                    <FormLabel>服务器名称</FormLabel>
+                    <FormControl>
+                      <>
+                        <Input
+                          disabled={isLoading}
+                          className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                          placeholder="请 输 入 服 务 器 名 称 ^_^"
+                          {...field}
+                        />
+                        {form.formState.errors.name && (
+                          <p className="text-red-500 text-right">
+                            {form.formState.errors.name.message}
+                          </p>
+                        )}
+                      </>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button
+                variant="primary"
+                disabled={isLoading}
+                className="text-bold text-white tracking-widest"
+              >
+                保 存
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -49,7 +49,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            className="px-3 py-2 text-sm cursor-pointer"
+            onClick={() => onOpen("editServer", { server })}
+          >
             服务器设置
             <Settings className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
