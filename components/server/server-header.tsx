4302e2c3ceef9ea663b217eda376b78f065da97c
Haohe Zhu <2452466220@qq.com>
2024-03-21 16:49:37 +0800

    feat: 管理成员

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -58,7 +58,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            className="px-3 py-2 text-sm cursor-pointer"
+            onClick={() => onOpen("members", { server })}
+          >
             管理成员
             <Users className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
