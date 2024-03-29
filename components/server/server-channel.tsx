a44b545a102626f783982108cf3c7020a41f0413
Haohe Zhu <2452466220@qq.com>
2024-03-29 16:04:07 +0800

    feat: 重定向->Channel Page

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -30,7 +30,9 @@ export const ServerChannel = ({
   const Icon = iconMap[channel.type];
   return (
     <button
-      onClick={() => {}}
+      onClick={() => {
+        router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
+      }}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -51,13 +53,19 @@ export const ServerChannel = ({
           <ActionTooltip label="编辑">
             <Edit
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:to-zinc-300 transition"
-              onClick={() => onOpen("editChannel", { server, channel })}
+              onClick={(e) => {
+                e.stopPropagation();
+                onOpen("editChannel", { server, channel });
+              }}
             />
           </ActionTooltip>
           <ActionTooltip label="删除">
             <Trash
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:to-zinc-300 transition"
-              onClick={() => onOpen("deleteChannel", { server, channel })}
+              onClick={(e) => {
+                e.stopPropagation();
+                onOpen("deleteChannel", { server, channel });
+              }}
             />
           </ActionTooltip>
         </div>
