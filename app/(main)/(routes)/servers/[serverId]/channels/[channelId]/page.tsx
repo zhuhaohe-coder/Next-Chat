2b1ea9349b2dfb7dcb48f993def5ad545e86763b
Haohe Zhu <2452466220@qq.com>
2024-03-29 16:52:55 +0800

    feat: chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,51 @@
-const ChannelIdPage = () => {
-  return <div>ChannelIdPage</div>;
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({
+  params: { serverId, channelId },
+}: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      profileId: profile.id,
+      serverId,
+    },
+  });
+
+  if (!channel || !member) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
