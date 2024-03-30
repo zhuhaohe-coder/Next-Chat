1e5b5ebbebd906429066772928163305d1810cc5
Haohe Zhu <2452466220@qq.com>
2024-03-30 15:58:41 +0800

    feat: conversations setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
+import { UserAvatar } from "../user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -19,6 +20,9 @@ export const ChatHeader = ({
       <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="w-5 h-5 mr-2 text-zinc-500 dark:text-zinc-400" />
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="h-8 w-8 md:w-8 md:h-8 mr-2" />
       )}
       <p className="font-semibold text-black dark:text-white tracking-widest uppercase">
         {name}
